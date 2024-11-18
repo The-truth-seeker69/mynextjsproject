920f15ea3eea86200834509a5d20e1bf9598f77c
Wei Quan <67322838+The-truth-seeker69@users.noreply.github.com>
2024-11-18 22:02:19 +0800

    nigga

--- app/ui/dashboard/latestInvoice.tsx
+++ app/ui/dashboard/latestInvoice.tsx
@@ -4,17 +4,17 @@ import { merriweather } from '../font'
 
 export default function LatestInvoice() {
     const customersData = [
-        { custName: "nigga123", custEmail: "nigga23123@gmail.com", custInvAmount: "$" + 1000 },
-        { custName: "nigga", custEmail: "nigga@gmail.com", custInvAmount: "$" + 1000 },
-        { custName: "nigga1", custEmail: "nigga@gmail.com", custInvAmount: "$" + 1000 },
-        { custName: "nigga2", custEmail: "nigga123@gmail.com", custInvAmount: "$" + 1000 },
+        { id: 1, custName: "nigga123", custEmail: "nigga23123@gmail.com", custInvAmount: "$" + 1000 },
+        { id: 2, custName: "nigga", custEmail: "nigga@gmail.com", custInvAmount: "$" + 1000 },
+        { id: 3, custName: "nigga1", custEmail: "nigga@gmail.com", custInvAmount: "$" + 1000 },
+        { id: 4, custName: "nigga2", custEmail: "nigga123@gmail.com", custInvAmount: "$" + 1000 },
 
     ]
     return (
         <div className="rounded-xl bg-gray-50 p-6 shadow-sm w-full  ">
             <div className=" p-2 bg-white rounded-lg shadow-md">
                 {customersData.map((customer, index) => (
-                    <div className="flex justify-between p-5  items-center border-b border-gray-300 last:border-b-0 ">
+                    <div key={customer.id} className="flex justify-between p-5  items-center border-b border-gray-300 last:border-b-0 ">
                         <div>
                             <h3 className={`${merriweather.className}`}>{customer.custName}</h3>
                             <p className={`${merriweather.className} text-gray-500`}>{customer.custEmail}</p>
